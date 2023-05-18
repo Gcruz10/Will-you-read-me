ae8cdb11871a62a9e8ffa3d33ab875522f92d86b
Gcruz10 <gabrielcruz2335@gmail.com>
2023-05-17 23:26:20 -0400

    added prompts to index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,45 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'In detail describe your project.'
+    },
+    {
+        type: 'input',
+        name: 'installations',
+        message: 'What are some installations used in your project?'
+    },
+    {
+        type: 'confirm',
+        name: 'table',
+        message: 'Would you like to add a Table of Contents?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What usage information can you add?'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'What have you contribuited to the project?'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'How can you test your project?'
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
